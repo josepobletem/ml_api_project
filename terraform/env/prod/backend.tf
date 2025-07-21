9024b71ab8cf980ce4709489810ca865961a710e
jose <jose.pobletealumnos.usm.cl>
2025-07-20 23:35:53 -0400

    FIX: change in backen

--- terraform/env/prod/backend.tf
+++ terraform/env/prod/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "local" {
-    bucket = "terraform-state-ml-api"
-    prefix = "env/prod"
+    #bucket = "terraform-state-ml-api"
+    #prefix = "env/prod"
+    path = "terraform.tfstate"
   }
 }
